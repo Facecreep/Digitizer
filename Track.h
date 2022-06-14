9be3cc2216cf8e23f8a4cc5dab149d9dd7d419d2
Aleksandr Dmitriev <aleksandr.dmitriev@cern.ch>
2022-06-14 21:55:23 +0300

    Review of the readout part && added verbose mode

--- Track.h
+++ Track.h
@@ -14,21 +14,21 @@ public:
     //Track(Short_t plane, Short_t strip, Float_t timeL, Float_t timeR, Float_t time, Float_t widthL, Float_t widthR, Float_t width, Float_t x, Float_t y, Float_t z, Float_t timeT0, Float_t widthT0);
     virtual ~Track();
 
-    Float_t GetfFitParamMX()     const { return fFitParamMX; }
-    Float_t GetfFitParamMY()     const { return fFitParamMY; }
-    Float_t GetfFitParamNX()       const { return fFitParamNX; }
-    Float_t GetfFitParamNY()     const { return fFitParamNY; }
-    Float_t GetfTrackLength()     const { return fTrackLength; }
-    Float_t GetfFirstPointX()      const { return fFirstPointX; }
-    Float_t GetfFirstPointY()      const { return fFirstPointY; }
-    Float_t GetfFirstPointZ()      const { return fFirstPointZ; }
-    Float_t GetfStopPointX()      const { return fStopPointX; }
-    Float_t GetfStopPointY()      const { return fStopPointY; }
-    Float_t GetfStopPointZ()      const { return fStopPointZ; }
-    Float_t GetfCharge()      const { return fCharge; }
-    Float_t GetfTrackMomentumX()      const { return fTrackMomentumX; }
-    Float_t GetfTrackMomentumY()      const { return fTrackMomentumY; }
-    Float_t GetfTrackMomentumZ()      const { return fTrackMomentumZ; }
+    Double_t GetfFitParamMX()     const { return fFitParamMX; }
+    Double_t GetfFitParamMY()     const { return fFitParamMY; }
+    Double_t GetfFitParamNX()       const { return fFitParamNX; }
+    Double_t GetfFitParamNY()     const { return fFitParamNY; }
+    Double_t GetfTrackLength()     const { return fTrackLength; }
+    Double_t GetfFirstPointX()      const { return fFirstPointX; }
+    Double_t GetfFirstPointY()      const { return fFirstPointY; }
+    Double_t GetfFirstPointZ()      const { return fFirstPointZ; }
+    Double_t GetfStopPointX()      const { return fStopPointX; }
+    Double_t GetfStopPointY()      const { return fStopPointY; }
+    Double_t GetfStopPointZ()      const { return fStopPointZ; }
+    Short_t GetfCharge()      const { return fCharge; }
+    Double_t GetfTrackMomentumX()      const { return fTrackMomentumX; }
+    Double_t GetfTrackMomentumY()      const { return fTrackMomentumY; }
+    Double_t GetfTrackMomentumZ()      const { return fTrackMomentumZ; }
     
     // void SetParameters(Short_t plane, Short_t strip, Float_t timeL, Float_t timeR, Float_t time, Float_t widthL, Float_t widthR, Float_t width, Float_t x, Float_t y, Float_t z, Float_t timeT0, Float_t widthT0);
     // void SetPlane(Short_t v)      { fPlane = v; }
@@ -44,26 +44,26 @@ public:
     // void SetZ(Float_t v)          { fZ = v; }
     // void SetTimeT0(Float_t v)     { fTimeT0 = v; }
     // void SetWidthT0(Float_t v)    { fWidthT0 = v; }
-    
+
     void Clear();
     
 private: 
 
-    Float_t fFitParamMX;
-    Float_t fFitParamMY;
-    Float_t fFitParamNX;
-    Float_t fFitParamNY;
-    Float_t fTrackLength;
-    Float_t fFirstPointX;
-    Float_t fFirstPointY;
-    Float_t fFirstPointZ;
-    Float_t fStopPointX;
-    Float_t fStopPointY;
-    Float_t fStopPointZ;
-    Float_t fCharge;
-    Float_t fTrackMomentumX;
-    Float_t fTrackMomentumY;
-    Float_t fTrackMomentumZ;
+    Double_t fFitParamMX;
+    Double_t fFitParamMY;
+    Double_t fFitParamNX;
+    Double_t fFitParamNY;
+    Double_t fTrackLength;
+    Double_t fFirstPointX;
+    Double_t fFirstPointY;
+    Double_t fFirstPointZ;
+    Double_t fStopPointX;
+    Double_t fStopPointY;
+    Double_t fStopPointZ;
+    Short_t fCharge;
+    Double_t fTrackMomentumX;
+    Double_t fTrackMomentumY;
+    Double_t fTrackMomentumZ;
 
     ClassDef(Track, 2);
 };
